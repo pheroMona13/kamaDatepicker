0435652abbcc22bd007cda40682fbb2748daef9c
Hadi Kefayati <h.kefaiati2010@gmail.com>
2020-06-30 17:37:23 +0430

    update packages and fix security vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,61 @@
 const path = require("path"),
-  webpack = require("webpack"),
-  package = require("./package.json"),
-  UglifyJSPlugin = require("uglifyjs-webpack-plugin"),
-  ExtractTextPlugin = require("extract-text-webpack-plugin"),
-  extractSass = new ExtractTextPlugin({ filename: "kamadatepicker.min.css" }),
-  buildMode = "production"; // 'development' or 'production'
+	webpack = require("webpack"),
+	package = require("./package.json"),
+	MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+	OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+	buildMode = "production"; // 'development' or 'production'
 
 module.exports = {
-  entry: {
-    kamadatepicker: ["./src/kamadatepicker.js"]
-  },
-  output: {
-    filename: "[name].min.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: getPlugins(),
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: extractSass.extract({
-          use: [
-            {
-              loader: "css-loader",
-              options:
-                buildMode === "production"
-                  ? { minimize: true }
-                  : { sourceMap: true }
-            },
-            {
-              loader: "sass-loader",
-              options: buildMode === "production" ? {} : { sourceMap: true }
-            }
-          ]
-        })
-      },
-      {
-        test: /\.js$/,
-        use: {
-          loader: "babel-loader",
-          query: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+	mode: buildMode,
+	entry: {
+		kamadatepicker: ["./src/kamadatepicker.js"]
+	},
+	output: {
+		filename: "[name].min.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	plugins: getPlugins(),
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: buildMode === 'development',
+						},
+					},
+					'css-loader',
+					'sass-loader',
+				]
+			},
+			{
+				test: /\.js$/,
+				use: {
+					loader: "babel-loader",
+					query: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}
+		]
+	}
 };
 
 function getPlugins() {
-  let plugins = [extractSass];
+	let plugins = [new MiniCssExtractPlugin({ filename: "kamadatepicker.min.css" })];
 
-  if (buildMode === "production") {
-    plugins.push(new UglifyJSPlugin());
-    plugins.push(
-      new webpack.BannerPlugin(`${package.name} - version ${package.version}`)
-    );
-  } else if (buildMode === "development") {
-    plugins.push(
-      new webpack.BannerPlugin({ banner: "/* @ sourceURL=[name].js */", raw: true })
-    );
-  }
+	if (buildMode === "production") {
+		plugins.push(new webpack.BannerPlugin(`${package.name} - version ${package.version}`));
+		plugins.push(new OptimizeCssAssetsPlugin());
+	}
+	else if (buildMode === "development") {
+		plugins.push(
+			new webpack.BannerPlugin({ banner: "/* @ sourceURL=[name].js */", raw: true })
+		);
+	}
 
-  return plugins;
+	return plugins;
 }
